b331292b0fa9a70f0f6f092a51f2d184c2926c4f
Aakarsh Chopra <alcatrazz.627@gmail.com>
2019-08-08 22:29:46 +0530

    Rotation working

---
+++ src/components/CommandLog.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react'
+
+const CommandLog = () => {
+    return (
+        <div className="logwindow">
+            This is text
+        </div>
+    )
+}
+
+export default CommandLog;
--- src/components/Controls.jsx
+++ src/components/Controls.jsx
@@ -2,49 +2,94 @@ import React, { useState } from 'react';
 
 import _ from 'lodash';
 
-import { Switch, Grid, Container, Slider } from '@material-ui/core';
+import CommandLog from './CommandLog';
+
+import { Switch, Grid, Container, Slider, Input, Button } from '@material-ui/core';
+import { Paper, Table, TableHead, TableRow, TableBody, TableCell } from '@material-ui/core';
+
+
+const MIN = 0, MAX = 10000;
+const STEP = (MAX - MIN) / 10;
+
+const AXES_DEF = {
+    x: 'Pitch',
+    y: 'Roll',
+    z: 'Yaw',
+}
+
 
 const Controls = () => {
-    const MIN = 0, MAX = 10000;
-    const STEP = (MAX-MIN)/10;
-
-    const altitudeMarks = _.range(MIN, MAX+1, STEP).map(e => ({value: e, label: `${e.toString()}m` }));
+    const altitudeMarks = _.range(MIN, MAX + 1, STEP).map(e => ({ value: e, label: `${e.toString()}m` }));
 
     const [armed, setArmed] = useState(false);
 
+    const [axes, setAxes] = useState({
+        x: 0,
+        y: 0,
+        z: 0
+    })
+
+
+    const handleAxesUpdate = ({ target: { name, value } }) => {
+        setAxes({ ...axes, [name]: parseInt(value) })
+    }
+
+    const handleAxesReset = (name) => {
+        setAxes({ ...axes, [name]: 0 })
+        // console.log(name)
+    }
+
     return (
         <div>
-            <div className="text-center">
+            <div className="armBox">
                 <Switch checked={armed} onChange={() => setArmed(!armed)} value="isArmed" />
             </div>
 
             <Grid container>
-                <Grid item xs="4">
+                <Grid item lg={4} sm={12}>
                     <Container>
                         <div className="sliderContainer">
-
-                            <Slider
-                                aria-labelledby="discrete-slider-always"
-
-                                defaultValue={80}
-
-                                step={STEP}
-                                min={MIN}
-                                max={MAX}
-                                marks={altitudeMarks}
-
-                                orientation="vertical"
-                                valueLabelDisplay="on"
-
+                            <Slider disabled={!armed} marks={altitudeMarks}
+                                defaultValue={80} min={MIN} max={MAX}
                                 // valueLabelFormat={x=>((x).toString() + 'm')}
-
-                                // height="300"
-                            />
+                                orientation="vertical" valueLabelDisplay="on" />
                         </div>
                     </Container>
                 </Grid>
-                <Grid item xs="8">
-                    
+
+                <Grid item lg={8} sm={10}>
+                    <div style={{ padding: '0 20px' }}>
+                        <div className="tiltdrone" style={{transform: `rotateX(${axes.x}deg) rotateY(${axes.y}deg) rotateZ(${axes.z}deg)`}} />
+                    </div>
+                    {/* </Grid>
+                <Grid item lg={4} sm={6}> */}
+                    <Table>
+                        <Paper>
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell>Parameter</TableCell>
+                                    <TableCell>Value</TableCell>
+                                    <TableCell>Options</TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {/* d->external dimension(Pitch, Roll, Yaw), a->(x, y, z) */}
+                                {_.map(AXES_DEF, (d, a) => <TableRow key={a}>
+                                    <TableCell>{d}</TableCell>
+                                    <TableCell>
+                                        <Input type="number" name={a} value={axes[a]} onChange={handleAxesUpdate} />
+                                    </TableCell>
+                                    <TableCell><Button onClick={()=>handleAxesReset(a)} variant="outlined" color="secondary">Reset</Button></TableCell>
+                                </TableRow>)}
+
+                            </TableBody>
+                        </Paper>
+                    </Table>
+                    {/* {JSON.stringify(axes)} */}
+                </Grid>
+
+                <Grid item xs={12}>
+                    <CommandLog />
                 </Grid>
             </Grid>
         </div>
